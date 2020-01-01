f296a30099c5d360870f888947338715b82ac645
ogheneruno <orunosaduwa@gmail.com>
2020-01-01 11:34:20 +0100

    install redux-logger

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import Main from './src/main';
 
-export default function App() {
+export default function App(props) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <Main {...props}/>
     </View>
   );
 }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {View} from 'react-native';
-
+import createStore from './store/createStore';
+import AppContainer from './AppContainer';
 const Main = () => {
 
     const renderApp = () => {
-        const initialState = window.__INITIAL_STATE_;
+        const initialState = window.__INITIAL_STATE__;
         const store = createStore(initialState);
-        return {
-            
-        };
+        return <AppContainer store={store}/>
 
     }
 
