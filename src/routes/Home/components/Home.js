7a51d28658e97f275120e685af4152b3c75d9aad
ogheneruno <orunosaduwa@gmail.com>
2020-01-01 10:35:55 +0100

    update home conmponents and conatainers

---
+++ src/routes/Home/components/Home.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import {View, Text} from 'react-native';
+
+
+const Home = () => {
+    return (
+        <View style={{flex:1, justifyContent:"center", alignItems:'center'}}>
+              <Text>Hello World</Text>
+        </View>
+    )
+}
+
+
+export default Home;
